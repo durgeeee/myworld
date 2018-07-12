eb3f767d4582ec0a6a5f2175639ec00401e8cbd0
durgeeee <41140945+durgeeee@users.noreply.github.com>
2018-07-12 14:34:29 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,9 +1,9 @@
 x=input()
 if(ord(x)>=97 and ord(x)<=122):
-
-    print("Consonant")
-elif(x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
-  print ("Vowel")
-else:
+    if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
+        print ("Vowel")
+    else:
+        print("Consonant")
+else:     
       print ("invalid")
 

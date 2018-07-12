5e20a321dbf0f24ab08e58028922bb9db2db868d
durgeeee <41140945+durgeeee@users.noreply.github.com>
2018-07-12 14:40:58 +0530

    Update apl.py

--- apl.py
+++ apl.py
@@ -1,6 +1,6 @@
 x=input()
 if((ord(x)>=97 and ord(x)<=122)or (ord (x)>=65 and ord (x)<=90)):
-        print ("Alphabets")
+        print ("yes")
 else:     
-      print ("invalid")
+      print ("No")
 
